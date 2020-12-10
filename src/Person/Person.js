256ad1347212bf78837d0cc7de340bb2cae85553
Ravichandran Rathinam <z1130533@wowcorp.com.au>
2020-12-10 20:23:42 +1100

    initial commit

---
+++ src/Person/Person.js
@@ -0,0 +1,13 @@
+import react from 'react';
+
+const person = (props) => {
+    return (
+        <div>
+            <p>My name is {props.name} and my age is {props.age}</p>
+            <p>{props.children}</p>
+        </div>
+    );
+
+};
+
+export default person;
